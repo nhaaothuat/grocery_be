8702cb19c257f21bf42bbd81224ebdfcb0505634
M2 C1 <hienphannguyenxuan@gmail.com>
2025-08-21 08:31:10 +0700

    first commit

---
+++ src/main/java/com/example/grocery_be/GroceryBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.grocery_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryBeApplication.class, args);
+	}
+
+}
